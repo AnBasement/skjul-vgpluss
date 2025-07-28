bc1ad4736ecbed196bb3e48d17fdf7d65173cfed
AnBasement <159536871+AnBasement@users.noreply.github.com>
2025-07-28 22:55:31 +0200

    Update skjulBetalingsmur.user.js

--- skjulBetalingsmur.user.js
+++ skjulBetalingsmur.user.js
@@ -3,6 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.1
 // @description  Skjul artikler på VG som krever VG+
+// @author       AnBasement
 // @match        https://www.vg.no/*
 // @grant        none
 // ==/UserScript==
